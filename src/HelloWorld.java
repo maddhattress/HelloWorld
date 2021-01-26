c3e595cfc24e29c91f829059f222483c05bb080f
Jenn Dell <64788812+maddhattress@users.noreply.github.com>
2021-01-25 22:46:36 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+//		for (int i = 0; i < 10; i=i+2) {
+//			System.out.println("i: " + i);
+//			System.out.println("Hello World!");
+//		}
+		for (int i = 0; i < 10; i++) {
+			if ((i % 2) == 0) {
+				System.out.println("i: " + i);
+				System.out.println("Hello World!");
+			}
+		}
+	}
+
+}
